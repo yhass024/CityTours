8b98cead8ae4dc8faa0e6798512fd30bdcfc7cc4
Yusuf <yusuf@Yusufs-MacBook-Pro.local>
2020-07-06 22:02:26 -0400

    Updates

---
+++ precache-manifest.9389a97cc6f05a0329a12020ad9ddc74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da40b5f5dbb652228875e384fefd463f",
+    "url": "/CityTours/index.html"
+  },
+  {
+    "revision": "eaed691d22aeae0d39cb",
+    "url": "/CityTours/static/css/main.24a50b73.chunk.css"
+  },
+  {
+    "revision": "c039d575277354fc11a8",
+    "url": "/CityTours/static/js/2.1807f28e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CityTours/static/js/2.1807f28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaed691d22aeae0d39cb",
+    "url": "/CityTours/static/js/main.596b36bc.chunk.js"
+  },
+  {
+    "revision": "c1f4344b56e5211de675",
+    "url": "/CityTours/static/js/runtime-main.cf922110.js"
+  }
+]);
